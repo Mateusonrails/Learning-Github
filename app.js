94a71388b096e6389afe0c6234d8d2ecdce76156
Mateus-Bozzi <mateuspvz1@gmil.com>
2022-10-16 22:29:02 -0300

    adicionando função

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ console.log("Rodando sistema de cadastro para os games!");
 console.log("Showing all the log:");
 console.log("Olá tudo bem??");
 console.log("HELLOOOOO WORLD!!!!!!!");
+
+function mostraAnuncio() {
+    alert("OLÁ MEU CARO TUDO BEM??")
+}
